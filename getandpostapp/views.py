92e3f02d5d5fab5976b0a902e781d28ba09dd174
ramakrishna <ramakrishnasri02@gmail.com>
2018-10-30 00:18:29 +0530

    Initial commit

---
+++ getandpostapp/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.shortcuts import render
+from django.http import  HttpResponse
+
+# Create your views here.
+def getinput(request):
+    return  render(request,'getinput.html')
+def postinput(request):
+    return  render(request,'postinput.html')
+def add(request):
+    if request.method=="GET":
+        try:
+            a=request.GET['t1']
+            x=int(a)
+            b=request.GET['t2']
+            y=int(b)
+            z=x+y
+            return HttpResponse("<html><body bgcolor=cyan><h1>sum:"+str(z)+"</h1></body></html>")
+        except(ValueError):
+            return HttpResponse("invalid input")
+    else:
+        try:
+            a=request.POST['t1']
+            x=int(a)
+            b=request.POST['t2']
+            y=int(b)
+            z=x+y
+            return  HttpResponse("<html><body bgcolor=red><h1>sum:"+str(z)+"</h1></body></html>")
+        except(ValueError):
+            return  HttpResponse("invalid input")
+
